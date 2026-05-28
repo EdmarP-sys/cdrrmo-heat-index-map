9526d0665293705b8c2a49d78f516152b919aff9
edmarp-sys <pined@DESKTOP-UFCUT07.local>
2026-05-28 13:24:55 +0800

    Clean repository: remove duplicate folders, unused legend icons, and obsolete GIS shapefile layers

--- data/MICE_5.js
+++
@@ -1,1 +0,0 @@
-var json_MICE_5 = {"type":"FeatureCollection","name":"MICE_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- data/Motels_6.js
+++
@@ -1,1 +0,0 @@
-var json_Motels_6 = {"type":"FeatureCollection","name":"Motels_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
